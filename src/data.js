a566c4b5b172d6f5c65aa39430a46de3d457d5d2
dipeshkoirala <dipeshkoirala@gmail.com>
2020-05-17 19:37:36 -0400

    data.js created

---
+++ src/data.js
@@ -0,0 +1,52 @@
+const fastfood = [
+  {
+    id: 1,
+    url: "../Assets/2.jpeg",
+    name: "MCDonald's",
+    Description: "$- American - Fast Food -Burgers",
+    Amount: 5.99,
+    Time: "20-30 Min",
+  },
+  {
+    id: 2,
+    url: "../Assets/3.jpeg",
+    name: "sweetgreen",
+    Description: "$- Healthy-Salads",
+    Amount: 4.99,
+    Time: "30-45 Min",
+  },
+  {
+    id: 3,
+    url: "../Assets/4.jpeg",
+    name: "Starbucks",
+    Description: "$- Cafe-Coffee & Tea-Breakfast and Brunch",
+    Amount: 5.99,
+    Time: "30-45 Min",
+  },
+  {
+    id: 4,
+    url: "../Assets/2.jpeg",
+    name: "BurgerKing",
+
+    Description: "$- American -Burgers and mores",
+    Amount: 5.99,
+    Time: "20-30 Min",
+  },
+  {
+    id: 5,
+    name: "Dunkin",
+    url: "../Assets/dunkin.jpeg",
+    Description: "$- American - tea -Donut",
+    Amount: 6.99,
+    Time: "20-30 Min",
+  },
+  {
+    id: 6,
+    url: "../Assets/dunkin1.jpeg",
+    name: "Panda Express",
+    Description: "$- Chinese - Fast Food -Rice &Spatetti",
+    Amount: 8.99,
+    Time: "28-30 Min",
+  },
+];
+export { fastfood };
